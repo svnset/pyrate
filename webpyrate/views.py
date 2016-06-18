e9f8cc14b882ddacd05b0c49dccdfd3231b06850
svnset <r.lachmann@uni-koeln.de>
2016-06-18 21:35:39 +0200

    started project

--- webpyrate/views.py
+++ webpyrate/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from webpyrate.models import Question
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'webpyrate/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'webpyrate/detail.html', {'question': question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'webpyrate/results.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'webpyrate/vote.html', {'question': question})
