eac33e7efa8306a520084a5a5f536f51c70b10b5
svnset <r.lachmann@uni-koeln.de>
2016-06-18 20:16:46 +0200

    initial commit

---
+++ pyrate/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for pyrate project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pyrate.settings")
+
+application = get_wsgi_application()
---
+++ webpyrate/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WebpyrateConfig(AppConfig):
+    name = 'webpyrate'
