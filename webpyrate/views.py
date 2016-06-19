3be29f4464df8bb77fdfc6e93eb0db8674a0e1fc
svnset <r.lachmann@uni-koeln.de>
2016-06-20 00:36:04 +0200

    added bootstrap

--- webpyrate/views.py
+++ webpyrate/views.py
@@ -1,21 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from webpyrate.models import Question
+from django.views import generic
+
+
 
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'webpyrate/index.html', context)
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'webpyrate/detail.html', {'question': question})
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'webpyrate/results.html', {'question': question})
-
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'webpyrate/vote.html', {'question': question})
+class Index(generic.TemplateView):
+    template_name = 'webpyrate/index.html'
