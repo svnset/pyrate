2d099671f2f722bac13149d2767133c979deb2a0
svnset <r.lachmann@uni-koeln.de>
2016-06-20 13:02:00 +0200

    new layout

--- webpyrate/static/js/main.js
+++ webpyrate/static/js/main.js
@@ -1,110 +1,70 @@
-jQuery(function($) {'use strict',
+// Hello.
+//
+// This is The Scripts used for ___________ Theme
+//
+//
 
-	
-	// all Parallax Section
-	$(window).load(function(){'use strict',
-		$("#services").parallax("50%", 0.3);
-		$("#clients").parallax("50%", 0.3);
+function main() {
+
+(function () {
+   'use strict';
+
+   /*====================================
+    Page a Link Smooth Scrolling 
+    ======================================*/
+    $('a.page-scroll').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 900);
+            return false;
+          }
+        }
+      });
+
+    /*====================================
+    Menu Active Calling Scroll Spy
+    ======================================*/
+    $('body').scrollspy({ 
+      target: '.navmenu',
+      offset: 80,
+    });
+
+
+    /* ==============================================
+	Testimonial Slider
+	=============================================== */ 
+
+	$(document).ready(function() {
+	 
+	  $("#testimonial").owlCarousel({
+	 
+	      navigation : false, // Show next and prev buttons
+	      slideSpeed : 300,
+	      paginationSpeed : 400,
+	      singleItem:true,
+	      autoHeight : true
+	 
+	      // "singleItem:true" is a shortcut for:
+	      // items : 1, 
+	      // itemsDesktop : false,
+	      // itemsDesktopSmall : false,
+	      // itemsTablet: false,
+	      // itemsMobile : false
+	 
+	  });
+	 
 	});
-	
-	// portfolio filter
-	$(window).load(function(){'use strict',
-		$portfolio_selectors = $('.portfolio-filter >li>a');
-		if($portfolio_selectors!='undefined'){
-			$portfolio = $('.portfolio-items');
-			$portfolio.isotope({
-				itemSelector : '.col-sm-3',
-				layoutMode : 'fitRows'
-			});
-			
-			$portfolio_selectors.on('click', function(){
-				$portfolio_selectors.removeClass('active');
-				$(this).addClass('active');
-				var selector = $(this).attr('data-filter');
-				$portfolio.isotope({ filter: selector });
-				return false;
-			});
-		}
-	});
-	
-	//Pretty Photo
-	 $("a[data-gallery^='prettyPhoto']").prettyPhoto({
-	  social_tools: false
-	 });
+ 
+
+ 
 
 
-	// Contact form validation
-	var form = $('.contact-form');
-	form.submit(function () {'use strict',
-		$this = $(this);
-		$.post($(this).attr('action'), function(data) {
-			$this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
-		},'json');
-		return false;
-	});
+}());
 
 
-	// Navigation Scroll
-	$(window).scroll(function(event) {
-		Scroll();
-	});
-
-	$('.navbar-collapse ul li a').click(function() {  
-		$('html, body').animate({scrollTop: $(this.hash).offset().top - 79}, 1000);
-		return false;
-	});
-
-});
-
-// Preloder script
-jQuery(window).load(function(){'use strict';
-	$(".preloader").delay(1600).fadeOut("slow").remove();
-});
-
-//Preloder script
-jQuery(window).load(function(){'use strict';
-
-	// Slider Height
-	var slideHeight = $(window).height();
-	$('#home .carousel-inner .item, #home .video-container').css('height',slideHeight);
-
-	$(window).resize(function(){'use strict',
-		$('#home .carousel-inner .item, #home .video-container').css('height',slideHeight);
-	});
-
-});
-
-
-// User define function
-function Scroll() {
-	var contentTop      =   [];
-	var contentBottom   =   [];
-	var winTop      =   $(window).scrollTop();
-	var rangeTop    =   200;
-	var rangeBottom =   500;
-	$('.navbar-collapse').find('.scroll a').each(function(){
-		contentTop.push( $( $(this).attr('href') ).offset().top);
-		contentBottom.push( $( $(this).attr('href') ).offset().top + $( $(this).attr('href') ).height() );
-	})
-	$.each( contentTop, function(i){
-		if ( winTop > contentTop[i] - rangeTop ){
-			$('.navbar-collapse li.scroll')
-			.removeClass('active')
-			.eq(i).addClass('active');			
-		}
-	})
-
-};
-
-
-	// Skill bar Function
-
-	jQuery(document).ready(function(){
-	jQuery('.skillbar').each(function(){
-		jQuery(this).find('.skillbar-bar').animate({
-			width:jQuery(this).attr('data-percent')
-		},6000);
-	});
-});
-
-
+}
+main();
